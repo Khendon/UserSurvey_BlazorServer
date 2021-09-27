9b1926250287dd89d50c75e4aba671e5996246df
Dave Pollock <khendon22@gmail.com>
2021-09-27 22:16:47 +0100

    Initial form setup and bindings (No implementation for submission or results)

---
+++ Data/SurveyData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using static UserSurvey_BlazorServer.Data.SurveyEnums;
+
+namespace UserSurvey_BlazorServer.Data
+{
+    public class SurveyData
+    {
+
+        [Required]
+        public string Sex { get; set; }
+
+        [Required]
+        public string AgeGroup { get; set; }
+
+        [Required(ErrorMessage = "Pick your favourite Sci-Fi show.")]
+        public SciFi SciFi { get; set; }
+
+
+    }
+}
